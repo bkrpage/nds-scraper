fabac83626be51927464e5801b70ede6c5326f61
bkrpage <bkrpage94@gmail.com>
2016-08-16 00:02:46 +0100

    Added Jinja2 HTML templating and output + added name detagging

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from lxml import html
 from lxml.html import builder as E
+from jinja2 import Environment
 import requests
 import re
 import settings
@@ -8,6 +9,7 @@ BASE_URL = 'http://www.emuparadise.me'
 LIST_URL = settings.settings['listUrl']
 
 RELEASE_NO_REGEX = re.compile(r'(?<=Nintendo DS Release #)\d+')
+TAG_REGEX = re.compile(r'(\(.+?\))')
 
 IMG_BASE_URL = 'http://s.emuparadise.org/ndsbox1/'
 IMG_ICON_SUFFIX = 'i.gif'
@@ -35,11 +37,12 @@ def get_games():
     for elem in bucket_elems:
         for pattern in settings.settings['patterns']:
             if re.search(pattern, elem.text_content()):
+                name_detagged = re.sub(TAG_REGEX, '', elem.text_content())
                 game_page = requests.Session().get(BASE_URL + elem.attrib['href'])
-                king_dict = {}
-                game_details = {'name': elem.text_content(), 'link': BASE_URL + elem.attrib['href'] + '-download'}
 
-                king_dict = add_dict(king_dict, game_details)
+                game_details = {'name': name_detagged, 'link': BASE_URL + elem.attrib['href'] + '-download'}
+
+                king_dict = add_dict({}, game_details)
                 king_dict = add_dict(king_dict, get_ratings(game_page))
                 king_dict = add_dict(king_dict, get_pictures(game_page))
 
@@ -48,7 +51,7 @@ def get_games():
                 game_list.append(king_dict)
                 print('Added Game ' + str(count) + '/' + str(full_count) + ': ' + king_dict['name'])
 
-    print(game_list)
+    generate_html(game_list)
 
 
 def add_dict(initial, add_dict):
@@ -98,6 +101,32 @@ def get_pictures(game_page):
 
     return images_urls
 
+def generate_html(game_list):
+
+    HTML = """
+    <html>
+    <head>
+    <title>Test</title>
+    </head>
+    <body>
+    <table>
+        <thead>
+            <th> Name </th>
+            <th> Rating </th>
+            <th> Rating Count </th>
+        </thead>
+        {% for dict in game_list %}
+        <tr>
+            <td> {{ dict["name"] }} </td>
+            <td> {{ dict["rating"] }} </td>
+            <td> {{ dict["rating_count"] }} </td>
+        </tr>
+        {% endfor %}
+    </table>
+    </body>
+    </html>
+    """
+    print Environment().from_string(HTML).render(game_list=game_list)
 
 if __name__ == '__main__':
     get_games()
