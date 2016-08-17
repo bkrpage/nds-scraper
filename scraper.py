80111b4c295c0958972918350f37e2d671503d48
bkrpage <bkrpage94@gmail.com>
2016-08-18 00:38:37 +0100

    changed EU regex again - now compiled, added functionality to remove duplicates automatically and count this.

--- scraper.py
+++ scraper.py
@@ -6,10 +6,11 @@ import settings
 BASE_URL = 'http://www.emuparadise.me'
 LIST_URL = settings.settings['listUrl']
 
-RELEASE_NO_REGEX = re.compile(r'(?<=Nintendo DS Release #)\d+')
+EU_REGEX = re.compile('\((?i)(eu)\)|\((?i)(e)\)')
+DS_REGEX = re.compile('\b([ds|DS])\w+')
 TAG_REGEX = re.compile(r'(\(.+?\))')
 SPECIAL_REGEX = re.compile('[^A-Za-z0-9]+')
-DS_REGEX = re.compile('\b([ds|DS])\w+')
+RELEASE_NO_REGEX = re.compile(r'(?<=Nintendo DS Release #)\d+')
 
 IMG_BASE_URL = 'http://s.emuparadise.org/ndsbox1/'
 IMG_ICON_SUFFIX = 'i.gif'
@@ -26,44 +27,58 @@ def get_games():
     bucket_elems = tree.find_class('gamelist')
 
     game_list = []
-    full_count = 0
     count = 0
+    duplicate_count = 0
+
+    # prints count also
+    full_count = count_all(bucket_elems)
 
     for elem in bucket_elems:
-        if re.search(settings.settings['pattern'], elem.text_content()):
-            full_count += 1
-
-    print('Total of ' + str(full_count) + ' games')
-
-    for elem in bucket_elems:
-        if re.search(settings.settings['pattern'], elem.text_content()):
+        if re.search(EU_REGEX, elem.text_content()):
 
             name_detagged = re.sub(TAG_REGEX, '', elem.text_content())
             name_stripped = strip_special_lower(name_detagged)
 
-            game_details = {
-                'id': count,
-                'name': name_detagged,
-                'stripped_name': name_stripped,
-                'link': BASE_URL + elem.attrib['href'] + '-download'
-            }
+            # only adds if the game isn't already matching another
+            # Have NOT yet tested this on full list - could be VERY slow TODO
+            # But then again the time used on searching the list is probably saved in NOT getting the game ID
+            if not any(game['stripped_name'] == name_stripped for game in game_list):
+                game_details = {
+                    'id': count,
+                    'name': name_detagged,
+                    'stripped_name': name_stripped,
+                    'link': BASE_URL + elem.attrib['href'] + '-download'
+                }
 
-            game_page = requests.Session().get(BASE_URL + elem.attrib['href'])
+                game_page = requests.Session().get(BASE_URL + elem.attrib['href'])
 
-            king_dict = add_dict({}, game_details)
-            king_dict = add_dict(king_dict, get_ratings(game_page))
-            king_dict = add_dict(king_dict, get_pictures(game_page))
+                king_dict = add_dict({}, game_details)
+                king_dict = add_dict(king_dict, get_ratings(game_page))
+                king_dict = add_dict(king_dict, get_pictures(game_page))
 
-            count += 1
+                count += 1
 
-            game_list.append(king_dict)
-            print('Added Game ' + str(count) + '/' + str(full_count) + ': ' + king_dict['name'])
-            # print(game_details)
+                game_list.append(king_dict)
+                print('Added Game ' + str(count) + '/' + str(full_count) + ': ' + king_dict['name'])
+                # print(game_details)
+            else:
+                duplicate_count += 1
 
-        if count >= 50:  # Just to limit the amount of time between test runs - instead of getting all 2000+ links
-            break
+        #  if count >= 50:  # Just to limit the amount of time between test runs - instead of getting all 2000+ links
+        #      break
+
+    print '\nTOTAL DUPLICATES: ' + str(duplicate_count)
 
     return game_list
+
+
+def count_all(bucket_elems):
+    full_count = 0
+    for elem in bucket_elems:
+        if re.search(EU_REGEX, elem.text_content()):
+            full_count += 1
+    print('Total of ' + str(full_count) + ' games')
+    return full_count
 
 
 def add_dict(initial, add):
--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 
 settings = {
-    'pattern': '\((?i)(eu)\)|\((?i)(e)\)',
     'listUrl': '/Nintendo_DS_ROMs/List-All-Titles/32'
 }
